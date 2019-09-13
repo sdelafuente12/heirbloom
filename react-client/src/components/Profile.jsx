1c2a9b2400851cb5346af200a632836ae0e5fb77
sdelafuente12 <s.delafuente831@gmail.com>
2019-09-13 17:58:54 -0500

    merge

--- react-client/src/components/Profile.jsx
+++ react-client/src/components/Profile.jsx
@@ -1,69 +1,3 @@
-<<<<<<< HEAD
-// not much here for the time being... just a header... need it to set up routes
-
-import React, { Fragment } from "react";
-import NavBar from "./NavBar.jsx";
-import "../App.css";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  Row,
-  Col
-} from "reactstrap";
-
-const Profile = props => {
-  console.log("Profile PROPS", props);
-  const { username, email, zipcode } = props.user;
-
-  return (
-    <Fragment>
-      <NavBar user={props.user} />
-      <div className="bg-profile pt-5">
-        <Row className="mt-5">
-          <Col
-            xl={{ size: 4, offset: 7 }}
-            md={{ size: 5, offset: 6 }}
-            xs={{ size: 10, offset: 1 }}
-            className="mt-5"
-          >
-            <div className="profile-head">
-              <h3 className="ml-3 text-capitalize">
-                {`${username}'s`} Profile
-              </h3>
-            </div>
-            <div className="profile-body">
-              <p>
-                Username: {`${username}`}
-                <br />
-                Email: {`${email}`}
-                <br />
-                Zip Code: {`${zipcode}`}
-              </p>
-              <hr className="bg-white" />
-              <h6>Options</h6>
-              <Button className="card-button btn-block">
-                Edit your profile
-              </Button>
-
-              <Button className="card-button btn-block">
-                Show favorite recipes
-              </Button>
-              <Button className="card-button btn-block">Log Out</Button>
-            </div>
-            <hr />
-          </Col>
-        </Row>
-      </div>
-    </Fragment>
-  );
-};
-
-export default Profile;
-=======
 // not much here for the time being... just a header... need it to set up routes
 
 import React, { Fragment } from "react";
@@ -127,4 +61,3 @@ const Profile = props => {
 };
 
 export default Profile;
->>>>>>> 76d0b22bffac6622c81b806b2077181daeec6ad7
